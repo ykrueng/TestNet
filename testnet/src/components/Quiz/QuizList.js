88b2cc5e2cddc2bf0e7cb9a70c74ba6aff38b7b3
Holden B <holden.bucher.92@gmail.com>
2018-12-10 16:16:16 -0600

    added radio buttons

--- testnet/src/components/Quiz/QuizList.js
+++ testnet/src/components/Quiz/QuizList.js
@@ -1,27 +1,45 @@
 import React from "react";
 
-const QuizList = props => {
+class QuizList extends React.Component {
   // console.log(props.quizzes[0].id);
-  return (
-    <div className="quiz-list">
-      {props.quizzes.map(quiz => (
-        <div className="quiz-card" key={quiz.id}>
-          <h1>{quiz.title}</h1>
-          <p>{quiz.author}</p>
-          <ul>
-            {quiz.questions.map(q => (
-              <div key={q.question}>
-                <li>{q.question}</li>
-                {q.answers.map(ans => (
-                  <li key={ans}>{ans}</li>
-                ))}
-              </div>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
-};
+  state = {
+    selected: ""
+  };
+
+  handleChange = e => {
+    this.setState({ answer: e.target.value });
+  };
+  render() {
+    const { quizzes } = this.props;
+    return (
+      <div className="quiz-list">
+        {quizzes.map(quiz => (
+          <div className="quiz-card" key={quiz.id}>
+            <h1>{quiz.title}</h1>
+            <p>{quiz.author}</p>
+            <ul>
+              {quiz.questions.map(q => (
+                <div key={q.question}>
+                  <li>{q.question}</li>
+                  {q.answers.map((ans, index) => (
+                    <label key={ans + index}>
+                      <input
+                        type="radio"
+                        value={`option${index}`}
+                        onChange={this.handleChange}
+                        checked={this.state.selected === `option${index}`}
+                      />
+                      {ans}
+                    </label>
+                  ))}
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default QuizList;
