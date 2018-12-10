0c33cd276fe2d68adb6f6708eaf56498de3481ca
Holden B <holden.bucher.92@gmail.com>
2018-12-10 12:27:30 -0600

    added boilerplate for the index.js file. imported rootReducer from ../store/reducers

--- testnet/src/index.js
+++ testnet/src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import rootReducer from "./store/reducers";
 
-import App from './App';
+import thunk from "thunk";
+import { createStore, applyMiddleware } from "redux";
+import { BrowswerRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.querySelector("#root")
 );
