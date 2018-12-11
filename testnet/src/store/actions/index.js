54c054b070826893039ebf9c7e075ecf506f2920
Holden B <holden.bucher.92@gmail.com>
2018-12-10 22:04:18 -0600

    capitalized Authorization in actions/index.js

--- testnet/src/store/actions/index.js
+++ testnet/src/store/actions/index.js
@@ -1,12 +1,12 @@
-import study from '../../apis/react-study';
+import study from "../../apis/react-study";
 
-export const REGISTER_REQUEST = 'REGISTER_REQUEST';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAILURE = 'REGISTER_FAILURE';
+export const REGISTER_REQUEST = "REGISTER_REQUEST";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
 
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 export const QUIZZES_REQUEST = "QUIZZES_REQUEST";
 export const QUIZZES_SUCCESS = "QUIZZES_SUCCESS";
@@ -47,228 +47,217 @@ export const COMMENT_REQUEST = "COMMENT_REQUEST";
 export const COMMENT_SUCCESS = "COMMENT_SUCCESS";
 export const COMMENT_FAILURE = "COMMENT_FAILURE";
 
-export const register = (user) =>
-  dispatch => {
-    dispatch({ type: REGISTER_REQUEST });
+export const register = user => dispatch => {
+  dispatch({ type: REGISTER_REQUEST });
 
-    study
-      .post('/auth/register', user)
-      .then( res => {
-        dispatch({
-          type: REGISTER_SUCCESS,
-          payload: {
-            token: res.token,
-          }
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: REGISTER_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .post("/auth/register", user)
+    .then(res => {
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: {
+          token: res.token
+        }
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: REGISTER_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const login = (user) =>
-  dispatch => {
-    dispatch({ type: LOGIN_REQUEST });
+export const login = user => dispatch => {
+  dispatch({ type: LOGIN_REQUEST });
 
-    study
-      .post('/auth/login', user)
-      .then( res => {
-        dispatch({
-          type: LOGIN_SUCCESS,
-          payload: { token: res.data.token }
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: LOGIN_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .post("/auth/login", user)
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: { token: res.data.token }
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: LOGIN_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getQuizzes = () =>
-  dispatch => {
-    dispatch({ type: QUIZZES_REQUEST });
+export const getQuizzes = () => dispatch => {
+  dispatch({ type: QUIZZES_REQUEST });
 
-    study
-      .get('/quizzes')
-      .then( res => {
-        dispatch({
-          type: QUIZZES_SUCCESS,
-          payload: res.data
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: QUIZZES_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .get("/quizzes")
+    .then(res => {
+      dispatch({
+        type: QUIZZES_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: QUIZZES_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getQuizz = quizzId =>
-  dispatch => {
-    dispatch({ type: QUIZZ_REQUEST });
+export const getQuizz = quizzId => dispatch => {
+  dispatch({ type: QUIZZ_REQUEST });
 
-    study
-      .get(`/quizzes/${quizzId}`)
-      .then( res => {
-        dispatch({
-          type: QUIZZ_SUCCESS,
-          payload: res.data
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: QUIZZ_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .get(`/quizzes/${quizzId}`)
+    .then(res => {
+      dispatch({
+        type: QUIZZ_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: QUIZZ_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getTopics = () =>
-  dispatch => {
-    dispatch({ type: TOPICS_REQUEST });
+export const getTopics = () => dispatch => {
+  dispatch({ type: TOPICS_REQUEST });
 
-    study
-      .get(`/quizzes/topics`)
-      .then( res => {
-        dispatch({
-          type: TOPICS_SUCCESS,
-          payload: res.data
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: TOPICS_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .get(`/quizzes/topics`)
+    .then(res => {
+      dispatch({
+        type: TOPICS_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: TOPICS_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-  export const postQuizz = (quizz, token) =>
-  dispatch => {
-    dispatch({ type: POST_QUIZZ_REQUEST });
+export const postQuizz = (quizz, token) => dispatch => {
+  dispatch({ type: POST_QUIZZ_REQUEST });
 
-    study
-      .post('/quizzes', quizz, { authorization: token } )
-      .then( res => {
-        console.log(res);
-        dispatch({
-          type: POST_QUIZZ_SUCCESS,
-          payload: res.data
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: POST_QUIZZ_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .post("/quizzes", quizz, { Authorization: token })
+    .then(res => {
+      console.log(res);
+      dispatch({
+        type: POST_QUIZZ_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: POST_QUIZZ_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getQuestions = id =>
-  dispatch => {
-    dispatch({ type: QUESTIONS_REQUEST });
+export const getQuestions = id => dispatch => {
+  dispatch({ type: QUESTIONS_REQUEST });
 
-    study
-      .get(`/quizzes/${id}/questions`)
-      .then( res => {
-        dispatch({
-          type: QUESTIONS_SUCCESS,
-          payload: res.data
-        })
-      })
-      .catch( err => {
-        dispatch({
-          type: QUESTIONS_FAILURE,
-          payload: { err }
-        })
-      })
-  }
+  study
+    .get(`/quizzes/${id}/questions`)
+    .then(res => {
+      dispatch({
+        type: QUESTIONS_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: QUESTIONS_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-  /*
+/*
     Post action creators
   */
- export const getPosts = () =>
- dispatch => {
-   dispatch({ type: POSTS_REQUEST });
+export const getPosts = () => dispatch => {
+  dispatch({ type: POSTS_REQUEST });
 
-   study
-     .get(`/posts`)
-     .then( res => {
-       dispatch({
-         type: POSTS_SUCCESS,
-         payload: res.data
-       })
-     })
-     .catch( err => {
-       dispatch({
-         type: POSTS_FAILURE,
-         payload: { err }
-       })
-     })
- }
+  study
+    .get(`/posts`)
+    .then(res => {
+      dispatch({
+        type: POSTS_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: POSTS_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getPost = postId =>
- dispatch => {
-   dispatch({ type: POST_REQUEST });
+export const getPost = postId => dispatch => {
+  dispatch({ type: POST_REQUEST });
 
-   study
-     .get(`/posts/${postId}`)
-     .then( res => {
-       dispatch({
-         type: POST_SUCCESS,
-         payload: res.data
-       })
-     })
-     .catch( err => {
-       dispatch({
-         type: POST_FAILURE,
-         payload: { err }
-       })
-     })
- }
+  study
+    .get(`/posts/${postId}`)
+    .then(res => {
+      dispatch({
+        type: POST_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: POST_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getComments = postId =>
- dispatch => {
-   dispatch({ type: COMMENTS_REQUEST });
+export const getComments = postId => dispatch => {
+  dispatch({ type: COMMENTS_REQUEST });
 
-   study
-     .get(`/posts/${postId}/comments`)
-     .then( res => {
-       dispatch({
-         type: COMMENTS_SUCCESS,
-         payload: res.data
-       })
-     })
-     .catch( err => {
-       dispatch({
-         type: COMMENTS_FAILURE,
-         payload: { err }
-       })
-     })
- }
+  study
+    .get(`/posts/${postId}/comments`)
+    .then(res => {
+      dispatch({
+        type: COMMENTS_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: COMMENTS_FAILURE,
+        payload: { err }
+      });
+    });
+};
 
-export const getComment = (postId, commentId) =>
- dispatch => {
-   dispatch({ type: COMMENT_REQUEST });
+export const getComment = (postId, commentId) => dispatch => {
+  dispatch({ type: COMMENT_REQUEST });
 
-   study
-     .get(`/posts/${postId}/comments/${commentId}`)
-     .then( res => {
-       dispatch({
-         type: COMMENT_SUCCESS,
-         payload: res.data
-       })
-     })
-     .catch( err => {
-       dispatch({
-         type: COMMENT_FAILURE,
-         payload: { err }
-       })
-     })
- }
+  study
+    .get(`/posts/${postId}/comments/${commentId}`)
+    .then(res => {
+      dispatch({
+        type: COMMENT_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: COMMENT_FAILURE,
+        payload: { err }
+      });
+    });
+};
