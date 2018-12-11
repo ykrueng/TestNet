d4b1d8568ffb14f4e8718be1fb5ac429c88c1381
Holden B <holden.bucher.92@gmail.com>
2018-12-11 10:51:44 -0600

    NavBar semantic ui roughed-in

--- testnet/src/components/NavBar/index.js
+++ testnet/src/components/NavBar/index.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Menu, Input } from "semantic-ui-react";
 
-const NavBar = () => {
-  return (
-    <div className="nav-container">
-      <h1>Welcome to TestNet!</h1>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/quizzes">Quizzes</NavLink>
-        <NavLink to="/posts">Forum</NavLink>
-      </nav>
-    </div>
-  );
-};
+class NavBar extends React.Component {
+  state = {
+    active: "home"
+  };
+  handleChange = e => {
+    this.setState({ active: e.target.name });
+  };
+
+  render() {
+    const { active } = this.state;
+    return (
+      <Menu secondary>
+        <Menu.Item
+          name="home"
+          as={NavLink}
+          exact
+          to="/"
+          active={active === "home"}
+          onClick={this.handleChange}
+        />
+        <Menu.Item
+          name="quizzes"
+          as={NavLink}
+          to="/quizzes"
+          active={active === "quizzes"}
+          onClick={this.handleChange}
+        />
+        <Menu.Item
+          name="posts"
+          as={NavLink}
+          to="/posts"
+          active={active === "posts"}
+          onClick={this.handleChange}
+        />
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input icon="search" placeholder="Search..." />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
 
 export default NavBar;
