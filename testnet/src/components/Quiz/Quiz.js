a031cbce5278625ec091c85799e758ec083ced2c
Holden B <holden.bucher.92@gmail.com>
2018-12-10 18:03:51 -0600

    page through quiz questions now

--- testnet/src/components/Quiz/Quiz.js
+++ testnet/src/components/Quiz/Quiz.js
@@ -1,36 +1,15 @@
 import React from "react";
 
 class Quiz extends React.Component {
-  state = {
-    selected: ""
-  };
-
-  handleChange = e => {
-    this.setState({ selected: e.target.value });
-  };
   render() {
     const title = this.props.match.params.title;
     const quiz = this.props.quizzes.find(quiz => quiz.title === title);
     return (
       <div className="quiz">
-        <ul>
-          {quiz.questions.map(q => (
-            <div key={q.question}>
-              <li>{q.question}</li>
-              {q.answers.map((ans, index) => (
-                <label key={ans + index}>
-                  <input
-                    type="radio"
-                    value={`option${index}`}
-                    onChange={this.handleChange}
-                    checked={this.state.selected === `option${index}`}
-                  />
-                  {ans}
-                </label>
-              ))}
-            </div>
-          ))}
-        </ul>
+        <h2>{quiz.title}</h2>
+        <button onClick={() => this.props.history.push(`/quizzes/${title}/1`)}>
+          Begin Quiz
+        </button>
       </div>
     );
   }
