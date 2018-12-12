6788134945965ff182c6277c79bbc7acfad4e924
Holden B <holden.bucher.92@gmail.com>
2018-12-12 16:12:20 -0600

    added logic to display all questions and check the selected answer

--- testnet/src/components/Quiz/Review.js
+++ testnet/src/components/Quiz/Review.js
@@ -8,21 +8,34 @@ const Review = props => {
   // console.log(answers);
   // console.log(rubric);
   // console.groupEnd();
+  let result = [];
+  for (let i = 0; i < rubric.length; i++) {
+    result.push(answers.find(item => item.question === questions[i].question));
+  }
   console.log(props);
+  console.log(result);
   const { id } = props.match.params;
   return (
-    <Grid centered columns={5}>
-      <Grid.Column width={4}>
+    <Grid centered columns={3} style={{ marginTop: "5rem" }}>
+      <Grid.Column>
         <Form>
-          {answers.map((pair, index) => {
+          {questions.map((q, index) => {
             return (
-              <Header key={index}>
-                {`${index + 1}. ${pair.question}`}
-                <Form.Radio label={pair.answer} checked={true} />
+              <Header key={index} style={{ marginBottom: "3rem" }}>
+                {`${index + 1}. ${q.question}`}
+                <Form.Group style={{ marginTop: "2rem" }}>
+                  {q.options.map((ans, i) => (
+                    <Form.Radio
+                      key={i}
+                      label={ans}
+                      checked={ans === answers[index] ? true : false}
+                    />
+                  ))}
+                </Form.Group>
               </Header>
             );
           })}
-          <Button.Group />
+
           <Button inverted color="green" content="Submit" attached="bottom" />
           <Button
             inverted
