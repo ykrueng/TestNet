3d97017678c54ee2cf353cc5c913700a42085c0c
Holden B <holden.bucher.92@gmail.com>
2018-12-12 14:31:31 -0600

    added Login modal handler

--- testnet/src/App.js
+++ testnet/src/App.js
@@ -4,13 +4,23 @@ import { connect } from "react-redux";
 import QuizView from "./views/QuizView";
 import PostList from "./components/Post/PostList";
 import DummyView from "./views/DummyView";
+import LoginForm from "./components/Login";
 import { login, register } from "./store/actions";
 
 class App extends React.Component {
+  state = {
+    modal: false
+  };
+
+  handleClick = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
   render() {
     return (
       <div>
-        <QuizView {...this.props} />
+        <QuizView {...this.props} click={this.handleClick} />
+        {this.state.modal && <LoginForm click={this.handleClick} />}
 
         <Route exact path="/dummy" render={props => <DummyView {...props} />} />
         <Route exact path="/posts" render={props => <PostList {...props} />} />
