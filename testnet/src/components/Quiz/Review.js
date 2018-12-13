49ed71f84a3be13afff518b57f5bc00900ca98a8
Holden B <holden.bucher.92@gmail.com>
2018-12-12 20:05:10 -0600

    changed imported component name

--- testnet/src/components/Quiz/Review.js
+++ testnet/src/components/Quiz/Review.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Overview from "./Overview";
+import Summary from "./Summary";
 import { Button, Form, Grid } from "semantic-ui-react";
 import { Redirect } from "react-router-dom";
 
 const Review = props => {
   const { questions, answers, rubric } = props;
   const { id } = props.match.params;
+  console.log(rubric);
   if (answers.length < 1) {
     return <Redirect to="/quizzes" />;
   }
@@ -13,7 +14,7 @@ const Review = props => {
     <Grid centered columns={3} style={{ marginTop: "5rem" }}>
       <Grid.Column>
         <Form>
-          <Overview questions={questions} answers={answers} />
+          <Summary questions={questions} answers={answers} />
           <Button inverted color="green" content="Submit" attached="bottom" />
           <Button
             inverted
