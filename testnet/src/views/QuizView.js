1ba85cb2ff36ac79c6c926870b1d062944270c53
Holden B <holden.bucher.92@gmail.com>
2018-12-12 23:59:40 -0600

    moved routes to separate file

--- testnet/src/views/QuizView.js
+++ testnet/src/views/QuizView.js
@@ -1,20 +1,18 @@
 import React from "react";
-import QuizList from "../components/Quiz/QuizList";
+import NavBar from "../components/NavBar";
+import QuizRoutes from "../components/Quiz/Routes";
 import { getQuizzes } from "../store/actions/quizzActions";
 import { connect } from "react-redux";
-import Quiz from "../components/Quiz/Quiz";
-import QuestionPage from "../components/Quiz/QuestionPage";
-import { Route } from "react-router-dom";
-import NavBar from "../components/NavBar";
-import Welcome from "../components/Welcome";
 
 class QuizView extends React.Component {
   state = {
     isAuthed: false
   };
+
   componentDidMount() {
     this.props.getQuizzes();
   }
+
   render() {
     const { isAuthed } = this.state;
     return (
@@ -27,22 +25,7 @@ class QuizView extends React.Component {
             placeholder="logged in comment area"
           />
         )}
-        <Route exact path="/" component={Welcome} />
-        <Route
-          exact
-          path="/quizzes"
-          render={props => <QuizList {...props} quizzes={this.props.quizzes} />}
-        />
-        <Route
-          exact
-          path={`/quizzes/:id`}
-          render={props => <Quiz {...props} />}
-        />
-        <Route
-          exact
-          path="/quizzes/:id/:questionId"
-          render={props => <QuestionPage {...props} />}
-        />
+        <QuizRoutes quizzes={this.props.quizzes} />
       </div>
     );
   }
