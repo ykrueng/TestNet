f11fc49506d0d5df4a663d568cd1f3f89e7085cc
Holden B <holden.bucher.92@gmail.com>
2018-12-13 00:00:19 -0600

    added conditonal rendering for final answers

--- testnet/src/components/Quiz/Review.js
+++ testnet/src/components/Quiz/Review.js
@@ -3,31 +3,50 @@ import Summary from "./Summary";
 import { Button, Form, Grid } from "semantic-ui-react";
 import { Redirect } from "react-router-dom";
 
-const Review = props => {
-  const { questions, answers, rubric } = props;
-  const { id } = props.match.params;
-  console.log(rubric);
-  if (answers.length < 1) {
-    return <Redirect to="/quizzes" />;
-  }
-  return (
-    <Grid centered columns={3} style={{ marginTop: "5rem" }}>
-      <Grid.Column>
-        <Form>
-          <Summary questions={questions} answers={answers} />
-          <Button inverted color="green" content="Submit" attached="bottom" />
-          <Button
-            inverted
-            attached="bottom"
-            color="red"
-            content="Restart"
-            onClick={() => props.history.push(`/quizzes/${id}`)}
-            style={{ marginTop: "1rem" }}
-          />
-        </Form>
-      </Grid.Column>
-    </Grid>
-  );
-};
+class Review extends React.Component {
+  state = {
+    reveal: false
+  };
 
+  reveal = e => {
+    e.preventDefault();
+    this.setState({ reveal: !this.state.reveal });
+  };
+  render() {
+    const { questions, answers, rubric, history } = this.props;
+    // const { id } = props.match.params;
+
+    if (answers.length < 1) {
+      return <Redirect to="/quizzes" />;
+    }
+
+    if (this.state.reveal) {
+      return <Summary questions={questions} answer={answers} rubric={rubric} />;
+    }
+    return (
+      <Grid centered columns={3} style={{ marginTop: "5rem" }}>
+        <Grid.Column>
+          <Form>
+            <Summary questions={questions} answers={answers} />
+            <Button
+              inverted
+              color="green"
+              content="Submit"
+              attached="bottom"
+              onClick={e => this.reveal(e)}
+            />
+            <Button
+              inverted
+              attached="bottom"
+              color="red"
+              content="Restart"
+              onClick={() => history.push(`/quizzes/${1}`)}
+              style={{ marginTop: "1rem" }}
+            />
+          </Form>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 export default Review;
