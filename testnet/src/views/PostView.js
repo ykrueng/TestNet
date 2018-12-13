40ee4ebcfbf36e516df1a70935e825d96fda155d
Holden B <holden.bucher.92@gmail.com>
2018-12-13 11:21:07 -0600

    bring in new route and wire up redux store

--- testnet/src/views/PostView.js
+++ testnet/src/views/PostView.js
@@ -1,14 +1,38 @@
 import React from "react";
 import PostList from "../components/Post/PostList";
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+import CommentSection from "../components/Post/Comments";
+import { getPosts } from "../store/actions/postActions";
 
 class PostView extends React.Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    console.log(this.props.posts);
     return (
       <div className="posts-container">
         <PostList posts={this.props.posts} />
+
+        <Route
+          path="/posts/:id"
+          render={props => <CommentSection {...props} />}
+        />
       </div>
     );
   }
 }
 
-export default PostView;
+const mapStateToProps = state => {
+  const { postReducer } = state;
+  return {
+    posts: postReducer.posts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getPosts }
+)(PostView);
