bbdaf697be71cece8524bbbda0efcb2808a65426
Holden B <holden.bucher.92@gmail.com>
2018-12-12 22:40:57 -0600

    made separate Quiz Routes file for less clutter

---
+++ testnet/src/components/Quiz/Routes/index.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
+import Welcome from "../../Welcome";
+import QuizList from "../QuizList";
+import Quiz from "../Quiz";
+import QuestionPage from "../QuestionPage";
+
+const QuizRoutes = ({ quizzes }) => {
+  return (
+    <Fragment>
+      <Route exact path="/" component={Welcome} />
+      <Route
+        exact
+        path="/quizzes"
+        render={props => <QuizList {...props} quizzes={quizzes} />}
+      />
+      <Route
+        exact
+        path={`/quizzes/:id`}
+        render={props => <Quiz {...props} />}
+      />
+      <Route
+        exact
+        path="/quizzes/:id/:questionId"
+        render={props => <QuestionPage {...props} />}
+      />
+    </Fragment>
+  );
+};
+
+export default QuizRoutes;
