7a70ecb58376a07b621f09134a3c54ab555a1a0a
Holden B <holden.bucher.92@gmail.com>
2018-12-19 14:18:19 -0600

    worked on SinglePost component to add in new changes

--- testnet/src/components/Post/SinglePost.js
+++ testnet/src/components/Post/SinglePost.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { getPost } from "../../store/actions/postActions";
+import { Header, Container, Button } from "semantic-ui-react";
+// import { getComments } from "../../store/actions/postActions";
+// import CommentSection from "./Comments";
 
 class SinglePost extends React.Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+    // this.props.getComments(this.props.match.params.id);
+  }
 
   render() {
+    const { post, token } = this.props;
+    const { id } = this.props.match.params;
     return (
-      <Segment>
-        <Header>
-          Title
-          <Header.Subheader>Created At:</Header.Subheader>
-        </Header>
-      </Segment>
+      post && (
+        <Fragment>
+          <Header as="h2" color="teal" block>
+            {post.title}
+            <Header.Subheader>Created At:{post.created_at}</Header.Subheader>
+          </Header>
+          <Container>{post.body}</Container>
+          <Button
+            onClick={() => this.props.history.push(`/posts/${id}/comments`)}
+          >
+            Show Comments
+          </Button>
+          {/* <CommentSection
+            comments={comments}
+            token={token}
+            match={this.props.match}
+          /> */}
+        </Fragment>
+      )
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { postReducer } = state;
+  const { postReducer, loginReducer } = state;
   return {
-    post: postReducer.post
+    post: postReducer.post,
+    token: loginReducer.token
   };
 };
 
