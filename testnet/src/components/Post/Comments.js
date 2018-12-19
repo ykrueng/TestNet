0ee7f75820edf4bc404d84c3fcc6204668165800
Holden B <holden.bucher.92@gmail.com>
2018-12-19 16:22:59 -0600

    added postComment action, controlled input, etc..

--- testnet/src/components/Post/Comments.js
+++ testnet/src/components/Post/Comments.js
@@ -2,31 +2,69 @@ import React from "react";
 import SingleComment from "./SingleComment";
 
 import { connect } from "react-redux";
-import { Button, Comment, Form } from "semantic-ui-react";
-import { getComments } from "../../store/actions/postActions";
+import { Button, Comment, Form, Segment } from "semantic-ui-react";
+import { getComments, postComment } from "../../store/actions/postActions";
 
 class CommentSection extends React.Component {
+  state = {
+    text: ""
+  };
+
   componentDidMount() {
     this.props.getComments(this.props.match.params.id);
   }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  postComment = () => {
+    const { token, postComment } = this.props;
+    const { id } = this.props.match.params;
+    postComment(id, this.state, token);
+    // this.setState({ text: "" });
+    this.props.history.push(`/posts/${id}`);
+  };
+
   render() {
     const { comments } = this.props;
+    const { id } = this.props.match.params;
     return (
-      <Comment.Group>
-        {comments.map((comment, i) => (
-          <SingleComment key={i} com={comment} />
-        ))}
+      <Segment>
+        <Comment.Group>
+          {comments.map((comment, i) => (
+            <Comment
+              key={i}
+              onClick={() =>
+                this.props.history.push(`/posts/${id}/comments/${comment.id}`)
+              }
+            >
+              <Comment.Content>
+                <Comment.Author>{comment.author}</Comment.Author>
+                <Comment.Metadata>
+                  <div>{comment.created_at}</div>
+                </Comment.Metadata>
+                <Comment.Text>{comment.text}</Comment.Text>
+              </Comment.Content>
+            </Comment>
+          ))}
 
-        <Form reply>
-          <Form.TextArea />
-          <Button
-            content="Add Reply"
-            labelPosition="left"
-            icon="edit"
-            primary
-          />
-        </Form>
-      </Comment.Group>
+          <Form reply>
+            <Form.TextArea
+              value={this.state.text}
+              onChange={this.handleChange}
+            />
+            <Button
+              content="Add Reply"
+              type="submit"
+              labelPosition="left"
+              icon="edit"
+              onClick={this.postComment}
+              primary
+            />
+          </Form>
+        </Comment.Group>
+      </Segment>
     );
   }
 }
@@ -41,5 +79,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { getComments }
+  { getComments, postComment }
 )(CommentSection);
