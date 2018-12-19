945e875a7af907a66ebb3627c982251dc709e0e3
Holden B <holden.bucher.92@gmail.com>
2018-12-19 16:21:43 -0600

    added SingleComment route

--- testnet/src/components/Post/routes/index.js
+++ testnet/src/components/Post/routes/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
 import Forum from "../../../views/Forum";
 import SinglePost from "../SinglePost";
+import SingleComment from "../SingleComment";
 import CommentSection from "../Comments";
 
 const PostRoutes = () => {
@@ -13,6 +14,11 @@ const PostRoutes = () => {
         exact
         path="/posts/:id/comments"
         render={props => <CommentSection {...props} />}
+      />
+      <Route
+        exact
+        path="/posts/:id/comments/:commentId"
+        render={props => <SingleComment {...props} />}
       />
     </Fragment>
   );
