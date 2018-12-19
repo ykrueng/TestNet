fb50594f45311be27b8170b3f796898e294cc2e9
Holden B <holden.bucher.92@gmail.com>
2018-12-19 16:22:41 -0600

    added lots of logic for edit/delete actions

--- testnet/src/components/Post/SingleComment.js
+++ testnet/src/components/Post/SingleComment.js
@@ -1,47 +1,99 @@
 import React from "react";
-import { Comment } from "semantic-ui-react";
+import { Comment, Form, Button } from "semantic-ui-react";
 import { connect } from "react-redux";
-import { getComment } from "../../store/actions/postActions";
+import {
+  getComment,
+  updateComment,
+  deleteComment
+} from "../../store/actions/postActions";
 
 class SingleComment extends React.Component {
-  // state = {
-  //   comment: ""
-  // };
+  state = {
+    reveal: false,
+    text: ""
+  };
+
   componentDidMount() {
-    this.props.getComment(this.props.com.post_id, this.props.com.id);
-    // this.setState({ comment: this.props.comment });
+    const { id, commentId } = this.props.match.params;
+    this.props.getComment(id, commentId);
   }
 
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  revealer = e => {
+    e.preventDefault();
+    this.setState({ reveal: !this.state.reveal });
+  };
+
+  editComment = () => {
+    const comment = { text: this.state.text };
+
+    const { id, commentId } = this.props.match.params;
+    this.props.updateComment(id, commentId, comment, this.props.token);
+    this.props.history.push(`/posts/${id}`);
+  };
+
+  deleteComment = () => {
+    const { id, commentId } = this.props.match.params;
+    this.props.deleteComment(id, commentId, this.props.token);
+    this.props.history.push(`/posts/${id}`);
+  };
+
   render() {
-    const { comment, com } = this.props;
+    const { comment } = this.props;
+
     return (
       comment && (
-        <Comment>
-          <Comment.Avatar src={comment.author.img_url} />
-          <Comment.Content>
-            <Comment.Author>{com.author}</Comment.Author>
-            <Comment.Metadata>
-              <div>{com.created_at}</div>
-            </Comment.Metadata>
-            <Comment.Text>{com.text}</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
+        <Comment.Group>
+          <Comment>
+            <Comment.Avatar src={comment.author.img_url} />
+            <Comment.Content>
+              <Comment.Author>{comment.author.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>{comment.created_at}</div>
+              </Comment.Metadata>
+              <Comment.Text>{comment.text}</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action onClick={this.revealer}>Edit</Comment.Action>
+                <Comment.Action onClick={this.deleteComment}>
+                  Delete
+                </Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+
+          {this.state.reveal && (
+            <Form reply>
+              <Form.TextArea
+                value={this.state.comment}
+                onChange={this.handleChange}
+              />
+              <Button
+                content="Edit Comment"
+                labelPosition="left"
+                icon="edit"
+                onClick={() => this.editComment()}
+                primary
+              />
+            </Form>
+          )}
+        </Comment.Group>
       )
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { postReducer } = state;
+  const { postReducer, loginReducer } = state;
   return {
-    comment: postReducer.comment
+    comment: postReducer.comment,
+    token: loginReducer.token
   };
 };
 
 export default connect(
   mapStateToProps,
-  { getComment }
+  { getComment, updateComment, deleteComment }
 )(SingleComment);
