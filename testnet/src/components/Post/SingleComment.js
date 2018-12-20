f0d8ece69536c70ccc6ecbd7bc505c5596315138
Holden B <holden.bucher.92@gmail.com>
2018-12-20 17:46:45 -0600

    added formatting and changed endpoint to send user to after submission

--- testnet/src/components/Post/SingleComment.js
+++ testnet/src/components/Post/SingleComment.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Comment, Form, Button } from "semantic-ui-react";
+import { Comment, Form, Button, Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
 import {
   getComment,
@@ -31,54 +31,60 @@ class SingleComment extends React.Component {
     const comment = { text: this.state.text };
     const { id, commentId } = this.props.match.params;
     this.props.updateComment(id, commentId, comment, this.props.token);
-    this.props.history.push(`/posts/${id}`);
+    this.props.history.push(`/posts/${id}/comments`);
   };
 
   deleteComment = () => {
     const { id, commentId } = this.props.match.params;
     this.props.deleteComment(id, commentId, this.props.token);
-    this.props.history.push(`/posts/${id}`);
+    this.props.history.push(`/posts/${id}/comments`);
   };
 
   render() {
     const { comment, user } = this.props;
     return (
       comment && (
-        <Comment.Group>
-          <Comment>
-            <Comment.Avatar src={comment.author.img_url} />
-            <Comment.Content>
-              <Comment.Author>{comment.author.username}</Comment.Author>
-              <Comment.Metadata>
-                <div>{comment.created_at}</div>
-              </Comment.Metadata>
-              <Comment.Text>{comment.text}</Comment.Text>
-              {user.id === comment.author.id && (
-                <Comment.Actions>
-                  <Comment.Action onClick={this.revealer}>Edit</Comment.Action>
-                  <Comment.Action onClick={this.deleteComment}>
-                    Delete
-                  </Comment.Action>
-                </Comment.Actions>
+        <Grid centered>
+          <Grid.Column>
+            <Comment.Group style={{ margin: "0 auto" }}>
+              <Comment>
+                <Comment.Avatar src={comment.author.img_url} />
+                <Comment.Content>
+                  <Comment.Author>{comment.author.username}</Comment.Author>
+                  <Comment.Metadata>
+                    <div>{comment.created_at}</div>
+                  </Comment.Metadata>
+                  <Comment.Text>{comment.text}</Comment.Text>
+                  {user.id === comment.author.id && (
+                    <Comment.Actions>
+                      <Comment.Action onClick={this.revealer}>
+                        Edit
+                      </Comment.Action>
+                      <Comment.Action onClick={this.deleteComment}>
+                        Delete
+                      </Comment.Action>
+                    </Comment.Actions>
+                  )}
+                </Comment.Content>
+              </Comment>
+              {this.state.reveal && (
+                <Form reply>
+                  <Form.TextArea
+                    value={this.state.comment}
+                    onChange={this.handleChange}
+                  />
+                  <Button
+                    content="Edit Comment"
+                    labelPosition="left"
+                    icon="edit"
+                    onClick={() => this.editComment()}
+                    primary
+                  />
+                </Form>
               )}
-            </Comment.Content>
-          </Comment>
-          {this.state.reveal && (
-            <Form reply>
-              <Form.TextArea
-                value={this.state.comment}
-                onChange={this.handleChange}
-              />
-              <Button
-                content="Edit Comment"
-                labelPosition="left"
-                icon="edit"
-                onClick={() => this.editComment()}
-                primary
-              />
-            </Form>
-          )}
-        </Comment.Group>
+            </Comment.Group>
+          </Grid.Column>
+        </Grid>
       )
     );
   }
