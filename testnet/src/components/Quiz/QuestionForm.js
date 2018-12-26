e0acd18d2718ff2ac886f5cb97da0a4255ac4583
ykrueng <yasirah.krueng@gmail.com>
2018-12-26 14:05:50 -0600

    refactor add question form

--- testnet/src/components/Quiz/QuestionForm.js
+++ testnet/src/components/Quiz/QuestionForm.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import { Segment, Form, Dropdown } from 'semantic-ui-react';
 
 class QuestionForm extends Component {
-  state = {  }
-  render() { 
+  state = {
+    question: '',
+    option1: '',
+    option2: '',
+    option3: '',
+    option4: '',
+    answer: null,
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  render() {
+    const { question, option1, option2, option3, option4, answer } = this.state;
+
     return (
       <Segment>
         <Form>
-          <Form.Input label="Question" />
+          <Form.Input
+            label="Question"
+            name="question"
+            value={question}
+            onChange={this.handleChange}
+          />
           <Form.Group widths="equal">
             <Form.Input
               label="Option 1"
-              name="title"
+              name="option1"
+              value={option1}
               onChange={this.handleChange}
               style={{
                 border: 'none'
@@ -19,39 +41,42 @@ class QuestionForm extends Component {
             />
             <Form.Input
               label="Option 2"
-              name="topic"
+              name="option2"
+              value={option2}
               onChange={this.handleChange}
-              style={{
-                border: 'none'
-              }}
             />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Input
               label="Option 3 - optional"
-              name="title"
+              name="option3"
+              value={option3}
               onChange={this.handleChange}
-              style={{
-                border: 'none'
-              }}
             />
             <Form.Input
               label="Option 4 - optional"
-              name="topic"
+              name="option4"
+              value={option4}
               onChange={this.handleChange}
-              style={{
-                border: 'none'
-              }}
             />
           </Form.Group>
-          <Dropdown placeholder='Answer Key' icon='key' floating labeled button className='icon'>
-            <Dropdown.Menu>
-              <Dropdown.Item text='Option 1' />
-              <Dropdown.Item text='Option 2' />
-              <Dropdown.Item text='Option 3' />
-              <Dropdown.Item text='Option 4' />
-            </Dropdown.Menu>
-          </Dropdown>
+          <Dropdown
+            placeholder='Answer Key'
+            icon='key' labeled button
+            className='icon'
+            selection
+            name="answer"
+            options={[
+              { key: "option1", value: "option1", text: "Option 1" },
+              { key: "option2", value: "option2", text: "Option 2" },
+              { key: "option3", value: "option3", text: "Option 3" },
+              { key: "option4", value: "option4", text: "Option 4" },
+            ]}
+            onChange={
+              (e, data) => this.setState({ [data.name]: data.value })
+            }
+          />
+
         </Form>
       </Segment>
     );
