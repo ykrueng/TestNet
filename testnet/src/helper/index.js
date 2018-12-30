0eb1d5ed149d6cdb391e3f55e2a9779b560a030a
ykrueng <yasirah.krueng@gmail.com>
2018-12-30 11:15:01 -0600

    refactor quizzes filter and sort to helper function

---
+++ testnet/src/helper/index.js
@@ -0,0 +1,44 @@
+export const filterQuizzes = (
+  quizzes,
+  activeOnly,
+  topics,
+  searchText,
+  searchField
+) => {
+  return quizzes.filter(quiz => {
+    if (activeOnly && quiz.question_count <= 0) {
+      return false;
+    }
+    if (topics.length > 0 && !topics.includes(quiz.topic)) {
+      return false;
+    }
+    if (!searchText) {
+      return true;
+    }
+    if (searchField === "all") {
+      return (
+        quiz.title.toLowerCase().includes(searchText.toLowerCase()) ||
+        quiz.topic.toLowerCase().includes(searchText.toLowerCase()) ||
+        quiz.author.toLowerCase().includes(searchText.toLowerCase())
+      );
+    }
+    return (
+      (topics.length === 0 || topics.includes(quiz.topic)) &&
+      quiz[searchField].toLowerCase().includes(searchText.toLowerCase())
+    );
+  });
+};
+
+export const sortQuizzes = (quizzes, sortingMethod) => {
+  return [...quizzes].sort((quizA, quizB) => {
+    const field = sortingMethod.includes("votes") ? "votes" : "question_count";
+    if (sortingMethod.includes("descending")) {
+      if (quizA[field] < quizB[field]) return 1;
+      if (quizA[field] === quizB[field]) return 0;
+      return -1;
+    }
+    if (quizA[field] < quizB[field]) return -1;
+    if (quizA[field] === quizB[field]) return 0;
+    return 1;
+  });
+};
